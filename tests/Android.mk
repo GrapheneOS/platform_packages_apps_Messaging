019fc1f23ad14668219b2d0d68d5b3a01842ff66
Bob Badour <bbadour@google.com>
2021-04-20 14:03:49 -0700

    [LSC] Add LOCAL_LICENSE_KINDS to packages/apps/Messaging
    Added SPDX-license-identifier-Apache-2.0 to:
      tests/Android.mk
    
    Added SPDX-license-identifier-Apache-2.0 SPDX-license-identifier-BSD
        SPDX-license-identifier-MIT
    to:
      Android.mk
    
    Bug: 68860345
    Bug: 151177513
    Bug: 151953481
    
    Test: m all
    
    Exempt-From-Owner-Approval: janitorial work
    Change-Id: I199886dd90a65c483a4f9782b5c77ffa5ecc3606

--- tests/Android.mk
+++ tests/Android.mk
@@ -21,6 +21,8 @@ LOCAL_MODULE_TAGS := tests
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := messagingtests
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0
+LOCAL_LICENSE_CONDITIONS := notice
 
 LOCAL_INSTRUMENTATION_FOR := messaging
 
